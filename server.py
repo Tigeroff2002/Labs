36af6ad6a4baa1849ccf49573fc8ec3a062b1e99
Tigeroff2002 <99354644+Tigeroff2002@users.noreply.github.com>
2023-06-07 21:02:46 +0300

    third upload

---
+++ server.py
@@ -0,0 +1,16 @@
+import socket 
+
+sock = socket.socket()
+sock.bind(('', 5000))
+sock.listen(10)
+print('Server is running...')
+clientsQueue = {}
+
+def checkPossibiltyToConnect(conn, adr):
+    return True
+
+print("Connected: 192.168.1.2:11587");
+print("Awaiting for disconnecting user and connecting others...")
+input()
+
+                    
